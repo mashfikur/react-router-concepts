236edac666b9bcc55ed176ed7bfff81cb214ae92
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-19 11:59:36 +0600

    react router link system added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
             <div className="text-center space-x-4 font-semibold my-6">
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/about"}>About</Link>
+                <Link to={"/contact"}>Contact</Link>
             </div>
         </div>
     );
