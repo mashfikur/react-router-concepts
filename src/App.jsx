5217e3d9083cb6aae1914f5e348f042528387d2f
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-20 00:05:26 +0600

    added useNavigation to add spinner on data loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import { FidgetSpinner } from "react-loader-spinner";
 
 function App() {
+  const navigation=useNavigation()
   return (
     <div className="text-center container mx-auto">
       <Navbar></Navbar>
       <h1 className="text-3xl text-center mb-6">This is the Permanent Header</h1>
-      <Outlet></Outlet>
+      {
+        navigation.state==="loading"
+        ?<FidgetSpinner
+        visible={true}
+        height="80"
+        width="200"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+        ballColors={['#ff0000', '#00ff00', '#0000ff']}
+        backgroundColor="#F4442E"
+      />
+      : <Outlet></Outlet>
+
+      }
+
 
       <h3 className="text-center text-2xl my-10">this is the footer of this website</h3>
     </div>
